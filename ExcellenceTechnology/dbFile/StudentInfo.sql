408d7cb805505152ca14712992011d8a2122e55e
CuriousInvention <69252900+CuriousInvention@users.noreply.github.com>
2021-08-28 19:07:20 +0530

    Delete ExcellenceTechnology/dbFile directory

--- ExcellenceTechnology/dbFile/StudentInfo.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE StudentInfo;
-
-USE DATABASE StudentInfo;
-
-CREATE TABLE user_info(uid int PRIMARY KEY AUTO_INCREMENT,fname varchar(20),lname varchar(20),email varchar(30),dob DATE,mobile varchar(12),designation varchar(20),gender varchar(10),hobbies varchar(50));
-
